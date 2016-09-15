1e40fe2c1de9c7f14495b98943f0e856f307a7ab
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-09-15 23:30:25 +0300

    Improvements and bugfixes on neighbouring

--- MarketKing.Game/GameConfig.cs
+++ MarketKing.Game/GameConfig.cs
@@ -7,5 +7,6 @@
         public const int StepSize = 30;
         public const int ThrottleRenderMiliseconds = 0;
         public const double HeightRatio = 2.4;
+        public const int FrameRate = 60;
     }
 }
